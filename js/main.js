31c6f4cdda36598ca00e7c058eb9493b759586cd
williamvg93 <sonico_5071@hotmail.com>
2023-08-27 00:55:53 -0500

    adding sidebar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log('Escuchandooo JS')
+let menuIcon = document.querySelector('.menu-icon')
+let sidebar = document.querySelector('.sidebar')
+
+console.log(sidebar);
+
+menuIcon.onclick = () => {
+    sidebar.classList.toggle("small-sidebar")
+}
