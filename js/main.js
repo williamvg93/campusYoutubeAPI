f405b90485b7f238a615018f880579c23ea9f616
williamvg93 <sonico_5071@hotmail.com>
2023-08-27 15:02:45 -0500

    adding the main content

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 let menuIcon = document.querySelector('.menu-icon')
 let sidebar = document.querySelector('.sidebar')
+let container = document.querySelector('.container')
 
 console.log(sidebar);
 
 menuIcon.onclick = () => {
     sidebar.classList.toggle("small-sidebar")
+    container.classList.toggle("large-container")
 }
