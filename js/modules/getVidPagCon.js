a42f6523c582daee7a5698e266e06d2eb37be879
williamvg93 <88460050+williamvg93@users.noreply.github.com>
2023-08-29 07:53:08 -0500

    Update VIdeo Page

--- js/modules/getVidPagCon.js
+++ js/modules/getVidPagCon.js
@@ -1,9 +1,10 @@
 let urlAva = ""
 let userName = ""
 
-export const getVidDet = async(typePet, urlPet, vidId) => {
+export const getVidDet = async(typePet, urlPet, vidId, vidViews) => {
 
     console.log(vidId);
+    console.log(vidViews);
 
     let peticion = ''
 
@@ -16,7 +17,7 @@ export const getVidDet = async(typePet, urlPet, vidId) => {
     let resp = await peticion.json();
     console.log(resp);
     console.log(peticion);
-    console.log(resp.keywords.length);
+    
 
     urlAva = resp.author.avatar[2].url
     userName = resp.author.title
@@ -32,8 +33,67 @@ export const getVidDet = async(typePet, urlPet, vidId) => {
     let contPlayVid = document.querySelector('#contPlayVid')
     console.log(contPlayVid)
     contPlayVid.insertAdjacentHTML("afterbegin", /* html */ `
-        <iframe src="https://www.youtube.com/embed/izvodnnCvt0?si=0LEHxTMcnFagHCYY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <iframe src="https://www.youtube.com/embed/izvodnnCvt0?si=0LEHxTMcnFagHCYY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     `)
+    
+     /* Agregando Tags del video  */
+    console.log(resp.keywords.length);
+    let mainVidTags = document.querySelector('#mainVidTags')
+    if (resp.keywords.length < 1 ){
+        contPlayVid.insertAdjacentHTML("beforeend", ``)
+    } else {
+        contPlayVid.insertAdjacentHTML("beforeend", /* html */ `
+            ${resp.keywords.map((vidTag) => /* html */ `
+                <a href="#"> ${vidTag} </a>
+            `)}
+        `)
+    }
+    
+    /* Agregando titulo del video principal  */
+    let titMaiVid = document.querySelector('#titMaiVid')
+    console.log(titMaiVid)
+    titMaiVid.insertAdjacentText('beforeend', /* html */ `
+        ${resp.title}
+    `)
+
+    /* Agregando views del video principal  */
+    let statsMainVid = document.querySelector('#statsMainVid')
+    console.log(statsMainVid)
+    statsMainVid.insertAdjacentHTML('beforeend', /* html */ `
+        ${resp.stats.views} views &bull; published ${vidViews}
+    `)
+
+    /* Agregando Likes del video principal  */
+    let likeMainVid = document.querySelector('#likeMainVid')
+    console.log(likeMainVid)
+    likeMainVid.insertAdjacentHTML('afterbegin', /* html */ `
+        <a href="#">
+            <img src="../img/like.png" alt="">${resp.stats.likes}</img>
+        </a>
+        <!--                         <a href="#">
+            <img src="../img/dislike.png" alt="">2</img>
+        </a> -->
+    `)
+
+    /* Agregando el avatar del video principal  */
+    let publisher = document.querySelector('#publisher')
+    let mainVidUserSus = document.querySelector('#mainVidUserSus')
+    let mainVidDes = document.querySelector('#mainVidDes')
+
+    publisher.insertAdjacentHTML('afterbegin', /* html */ `
+        <img src="${resp.author.avatar[2].url}" alt="">
+    `)
+    mainVidUserSus.insertAdjacentHTML('beforeend', /* html */ `
+        <p>${userName}</p>
+        <span>${resp.author.stats.subscribersText}</span>
+    `)
+
+    mainVidDes.insertAdjacentHtml('beforeend', /* html */ `
+    <p>${userName}</p>
+    <span>${resp.description}</span>
+`)
+
+
     
 }
 
